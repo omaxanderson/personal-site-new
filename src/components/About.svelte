10d0c6f6fa581213abe3ba9860088036b0f0c53e
O Max Anderson <omaxwellanderson@gmail.com>
2021-08-06 23:39:17 -0500

    init

---
+++ src/components/About.svelte
@@ -0,0 +1,1 @@
+Hi, my name is O Max and I love to code.
---
+++ src/components/Header.svelte
@@ -0,0 +1,55 @@
+<style>
+  .title-name {
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+
+  #header-img {
+    width: 100vw;
+    position: fixed;
+    z-index: -1;
+  }
+
+  .flex {
+    display: flex;
+  }
+
+  .justify-content {
+    justify-content: center;
+  }
+
+  .align-items {
+    align-items: center;
+  }
+
+  h1 {
+    position: absolute;
+    top: 20%;
+    color: white;
+  }
+</style>
+
+<script>
+    let img, imageHeight;
+    function onImageLoad() {
+        imageHeight = img.offsetHeight;
+        console.log(imageHeight);
+    }
+</script>
+
+<div class="title">
+  <div
+    class="flex justify-content align-items"
+    style="height: {imageHeight}px"
+  >
+    <img
+      id="header-img"
+      bind:this={img}
+      on:load={onImageLoad}
+      src="./static/seattle.jpg"
+      alt="Seattle from Pike Place"
+    />
+    <h1>O Max Anderson</h1>
+  </div>
+</div>
