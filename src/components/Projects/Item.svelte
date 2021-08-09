3a71cc3e21ba5ed91d50e3cf6805f311f91cb99d
O Max Anderson <omaxwellanderson@gmail.com>
2021-08-09 10:01:35 -0500

    add project link option

--- src/components/Projects/Item.svelte
+++ src/components/Projects/Item.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
     export let title: string;
-    export let link: string;
+    export let codeLink: string;
+    export let projectLink: string;
     export let description: string;
     export let techStack: string[];
 </script>
 
 <div class="container">
     <h4>{title}</h4>
-    <p><a href={link} target="_blank">See the code on GitHub</a></p>
+    <p><a href={codeLink} target="_blank">See the code on GitHub</a></p>
+    {#if projectLink}
+        <p><a href={projectLink} target="_blank">Go to project</a></p>
+    {/if}
     <p>{@html description}</p>
     <div class="tech-stack">
         <h5>Tech Stack</h5>
--- src/components/Projects/Projects.svelte
+++ src/components/Projects/Projects.svelte
@@ -4,7 +4,8 @@ import Item from "./Item.svelte";
     const projects = [
         {
             title: 'Online Werewolf Game',
-            link: 'http://github.com/omaxanderson/werewolf',
+            codeLink: 'http://github.com/omaxanderson/werewolf',
+            projectLink: 'http://omaxwellanderson.com:3000',
             description: `When COVID hit, my friends and I couldn't play one of our favorite games in-person any more,
                 so I decided to build an online version. After working out a few bugs the first time we played,
                 it has been a great way to be able to play games with friends again.`,
@@ -12,7 +13,7 @@ import Item from "./Item.svelte";
         },
         {
             title: 'Spotify playlist mixer',
-            link: 'http://github.com/omaxanderson/spotify-multi-playlist',
+            codeLink: 'http://github.com/omaxanderson/spotify-multi-playlist',
             description: `I make a lot of Spotify playlists and one day I had an idea that it would be nice to be able to listen
                 to two playlists at the same time, essentially "zipping" the playlist together so a song from the first
                 playlist would play, then the second playlist, possibly the third, and then repeat. Using Spotify's API
@@ -21,7 +22,7 @@ import Item from "./Item.svelte";
         },
         {
             title: 'Personal site',
-            link: 'http://github.com/omaxanderson/personal-site-new',
+            codeLink: 'http://github.com/omaxanderson/personal-site-new',
             description: `I re-built my personal site after nearly three years of being a professional engineer and used
                 this as an opportunity to learn something new: <a href="https://svelte.dev/">Svelte</a>.
                 This lightweight frontend framework acts as a compiler into vanilla HTML, CSS, and Javascript
